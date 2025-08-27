890e4c520f5a8cdf513c0f5a1859e0a8b30d8dcc
dante <dante.tarragau@gmail.com>
2025-08-27 00:24:02 -0500

    Añade dependencias de cookie-parser y cors, y elimina código comentado en la ruta de autenticación de TikTok

--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -3,9 +3,6 @@ import { Router } from 'express'
 import { AuthController } from '@/controllers'
 import { validateSchema } from '@/middleware'
 
-import { Request, Response } from 'express'
-import 'dotenv/config'
-
 import {
   loginSchema,
   registerSchema,
@@ -42,39 +39,5 @@ authRouter.post(
 authRouter.get('/tiktok', AuthController.tiktokLogin)
 
 authRouter.get('/tiktok/callback', AuthController.tiktokCallback)
-
-// const CLIENT_KEY = process.env.TIKTOK_CLIENT_KEY!
-// const CLIENT_SECRET = process.env.TIKTOK_CLIENT_SECRET!
-// const REDIRECT_URI = "https://social-test-eqq4.onrender.com/api/auth/tiktok/callback"; // cambia según tu dominio
-// const csrfState = Math.random().toString(36).substring(2);
-
-// console.log("Client Key:", CLIENT_KEY)
-// // Tik tok
-// // TikTok login
-// authRouter.get('/tiktok/login', (req, res) => {
-//     const csrfState = Math.random().toString(36).substring(2);
-//     res.cookie('csrfState', csrfState, { maxAge: 60000 });
-
-//     let url = 'https://www.tiktok.com/v2/auth/authorize/';
-
-
-//     // the following params need to be in `application/x-www-form-urlencoded` format.
-//     url += `?client_key=${CLIENT_KEY}`;
-//     url += `&scope=user.info.basic`;
-//     url += `&response_type=code`;
-//     url += `&redirect_uri=${REDIRECT_URI}`;
-//     url += `&state=${csrfState}`;
-
-
-//     res.redirect(url);
-
-// })
-
-
-// authRouter.get("/tiktok/callback", (req: Request, res: Response) => {
-//   const { code } = req.query;
-//   // Aquí manejarías el intercambio del código por un token de acceso
-//   res.send("Callback de TikTok");
-// });
 
 export default authRouter
