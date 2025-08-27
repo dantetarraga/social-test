43da91e29446c324723e8f4ba1f730e3fb93d8f7
dante <dante.tarragau@gmail.com>
2025-08-26 23:10:43 -0500

    Añade generación dinámica de estado CSRF en la URL de autenticación de TikTok

--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -42,12 +42,14 @@ authRouter.post(
 const CLIENT_KEY = process.env.TIKTOK_CLIENT_KEY!;
 const CLIENT_SECRET = process.env.TIKTOK_CLIENT_SECRET!;
 const REDIRECT_URI = "https://social-test-eqq4.onrender.com/api/auth/tiktok/callback"; // cambia según tu dominio
+const csrfState = Math.random().toString(36).substring(2);
 
 // Tik tok
 authRouter.get("/tiktok/login", (req: Request, res: Response) => {
-  const authUrl = `https://www.tiktok.com/v2/auth/authorize/?client_key=${CLIENT_KEY}&response_type=code&scope=user.info.basic&redirect_uri=${encodeURIComponent(
+  const authUrl = `https://www.tiktok.com/v2/auth/authorize?client_key=${CLIENT_KEY}&response_type=code&scope=user.info.basic&redirect_uri=${encodeURIComponent(
     REDIRECT_URI
-  )}&state=xyz123`; // "state" para evitar CSRF
+  )}&state=${csrfState}`; 
+  // "state" para evitar CSRF
   res.redirect(authUrl);
 });
 
