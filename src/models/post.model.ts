11d6e93f55464292a07a03ba00d20f277a9d3966
dante <dante.tarragau@gmail.com>
2025-09-01 20:56:16 -0500

    Añade soporte para el callback de Instagram, incluyendo la lógica de autenticación y guardado de conexiones.

--- src/models/post.model.ts
+++ src/models/post.model.ts
@@ -1,33 +1,22 @@
 import { Column, Entity, JoinTable, ManyToMany, PrimaryGeneratedColumn } from "typeorm";
 import { Profile } from "./profile.model";
-import { PublishingType } from "@/types";
 import { MediaItem } from "@/schema";
 
 @Entity()
 export class Post {
   @PrimaryGeneratedColumn()
-  id!: number
+  id!: number;
 
-  @Column({ type: 'text' })
-  content!: string
+  @Column({ type: "text" })
+  content!: string;
 
-  @Column({ type: 'simple-json', nullable: true })
-  media?: MediaItem[]
+  @Column({ type: "simple-json", nullable: true })
+  media?: MediaItem[];
 
-  @Column({
-    type: 'enum',
-    enum: PublishingType,
-    default: PublishingType.NOW,
-  })
-  publishing!: PublishingType
-
-  // @Column({ type: 'simple-array', nullable: true })
-  // permissions?: string[]
-
-  @Column({ type: 'timestamp', nullable: true })
-  scheduledAt?: Date
+  @Column({ type: "timestamp", nullable: true })
+  scheduledAt?: Date;
 
   @ManyToMany(() => Profile, (profile) => profile.posts, { cascade: true })
   @JoinTable()
-  profiles!: Profile[]
+  profiles!: Profile[];
 }
