b334c9362452846aa53a6295bd421b9824c71550
dante <dante.tarragau@gmail.com>
2025-09-14 16:59:06 -0500

    feat: eliminar rutas y esquemas de perfil, publicación y autenticación

--- src/schema/profile.schema.ts
+++
@@ -1,9 +0,0 @@
-import z from 'zod'
-
-export const createProfileSchema = z.object({
-  name: z.string('The name field is required').min(2).max(100),
-  description: z.string('The description field is required').min(2).max(100),
-  color: z.string().optional()
-})
-
-export const updateProfileSchema = createProfileSchema.partial()
